a79946cb60af885a627bc1012a274a33b0717993
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-06-28 11:06:54 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+// function scuberGreetingForFeet(feet){
+//   let distance;
+//   if (distance <=400); {
+//    console.log ("This one is on me!")
+//   }
+//  if (distance> 2000){
+//     console.log ('I will gladly take your thirty bucks.')
+//   }
+//   else if (distance>2500){
+//     console.log('No can do.')
+//   }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// }
+
+// function ternaryCheckCity(city){
+//   // Write your code here!
+//   let city
+//   if (city= NYC){
+//     console.log('OK, sounds good.')
+
+//   }
+//   else if (city= !NYC && city ==Pitsburgh){
+//     console.log('No Go.')
+
+//   }
+
+// }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let tip = ["generous", "!generous"]
+  if (tip== generous){
+    console.log('Thank you so much.')
+    
+  }
+  else if (tip == !generous) {
+    console.log('Thank you.')
+  }
+  
+  else {
+    console.log ('Bye') 
+      
+    
+  }
+  
+
 }
+
+switchOnCharmFromTip();
